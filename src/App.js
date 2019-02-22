f702bc9c5e4a85c13e9d6467453eb6330c0aa1b6
Laura Do <ldo@Lauras-Work-Mac.local>
2019-02-22 10:58:22 -0500

    set up deck logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import lauras from './lauras';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentLaura: {},
+      discoverLauras: lauras,
+      matchedLauras: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setCurrentLaura();
+  }
+
+  setCurrentLaura() {
+    if(this.state.discoverLauras.length > 0) {
+      const [first, ...rest] = this.state.discoverLauras;
+      this.setState({
+        discoverLauras: rest,
+        currentLaura: first,
+      })
+    }
+  }
+
   render() {
+    // TODO: add loading state when currentLaura is empty
+
     return (
       <div className="App">
         <header>
           pizza meets doughnut
         </header>
+        <div className='discover-container'>
+          <p>{this.state.currentLaura.name}</p>
+        </div>
       </div>
     );
   }
---
+++ src/lauras.js
@@ -0,0 +1,13 @@
+const lauras = [
+  {
+    name: 'Adventurous Laura'
+  },
+  {
+    name: 'Nerdy Laura'
+  },
+  {
+    name: 'Silly Laura'
+  },
+]
+
+export default lauras;
